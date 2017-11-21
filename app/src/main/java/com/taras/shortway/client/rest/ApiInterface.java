5b9bd6f777fbfaeaefcdd16c93f50527a779ce0c
Taras Rakochyi <taras.rakochyi@eleks.com>
2017-11-21 19:55:27 +0200

    add methods for requests to apiinterface

--- app/src/main/java/com/taras/shortway/client/rest/ApiInterface.java
+++ app/src/main/java/com/taras/shortway/client/rest/ApiInterface.java
@@ -1,12 +1,36 @@
 package com.taras.shortway.client.rest;
 
+import com.taras.shortway.client.model.Auto;
+import com.taras.shortway.client.model.Trip;
 import com.taras.shortway.client.model.User;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
     @GET("/loginpass")
     Call<User> getUserFromLogin(@Query("login") String login, @Query("password") String password);
+
+    @GET("/autos")
+    Call<List<Auto>> getAutos();
+
+    @GET("/autos/{id}")
+    Call<Auto> getAutoById(@Path("id") int id);
+
+    @GET("/users")
+    Call<List<User>> getUsers();
+
+    @GET("/users/{id}")
+    Call<User> getUserById(@Path("id") int id);
+
+    @GET("/trips")
+    Call<List<Trip>> getTrips();
+
+    @GET("/trips/{id}")
+    Call<Trip> getTripById(@Path("id") int id);
+
 }
