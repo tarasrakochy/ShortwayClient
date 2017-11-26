e6d63c69139d024624bb58eaa57f0fe69f4935de
tarkorti <tarkorti911@gmail.com>
2017-11-27 01:25:35 +0200

    comment old code

--- app/src/main/java/com/taras/shortway/client/TripTypeChooserActivity.java
+++ app/src/main/java/com/taras/shortway/client/TripTypeChooserActivity.java
@@ -16,11 +16,11 @@ public class TripTypeChooserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_type_chooser);
 
-        Button button = (Button) findViewById(R.id.log_out_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
+//        Button button = (Button) findViewById(R.id.log_out_button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//            }
+//        });
     }
 }
